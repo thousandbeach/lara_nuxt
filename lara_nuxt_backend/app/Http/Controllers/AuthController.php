eead463962285062a37f8228989aeaece9709bb7
takabo <thousandbeach.td@gmail.com>
2019-01-22 03:02:00 +0900

    make:resource User

--- lara_nuxt_backend/app/Http/Controllers/AuthController.php
+++ lara_nuxt_backend/app/Http/Controllers/AuthController.php
@@ -3,20 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Http\Requests\UserRegisterRequest;
 
 class AuthController extends Controller
 {
     //
-    public function register(Request $request){
-        // バリデーションとか書く
-        //dd('Register!!');
-        $this->validate($request, [
-            'name' => 'required|string|max:180',
-            'email' => 'required|string|email|max:180|unique:users,email',
-            'password' => 'required|min:6',
-        ]);
-
+    public function register(UserRegisterRequest $request){
+        // バリデーションとかはUserRegisterRequestに
+        //dd('来てるよ〜');
+        
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
